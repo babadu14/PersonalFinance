7e5778462510cf261fb8683f5e1fc18571a7aa07
babadu14 <andriamanagadze7@gmail.com>
2025-04-25 22:42:23 +0400

    added validation for budget

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from finance.models import Transaction, Budget
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from django.db.models import Value
+from django.db.models import DecimalField
+
 
 class BudgetSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +15,20 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
+
+    def validate(self, data):
+        budget = data['budget_category']
+        amount = data['amount']
+        type1 = data['type']
+
+        if type1 == 'Expense':
+            total_expense = Transaction.objects.filter(budget_category=budget,type = 'Expense').aggregate(
+                total=Coalesce(Sum('amount'), 
+                               Value(0), 
+                               output_field=DecimalField()))['total']
+        
+        if total_expense + amount > budget.limit:
+            raise serializers.ValidationError({"message":"budget is less than expense"})
+        
+        return data
+    
