c8564a1afe989465342bd6d65043fac6690598d8
babadu14 <andriamanagadze7@gmail.com>
2025-05-03 23:52:53 +0400

    views logic

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from users.serializers import (RegisterSerielizer, UserSerializer, 
+                               PasswordResetConfirmSerializer, ProfileSerializer,
+                               EmailCodeResendSerializer, )
 
 # Create your views here.
