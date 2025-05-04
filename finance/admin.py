f6448f7acac23999d52ddf2c1ef45fa2fe92ed5a
babadu14 <andriamanagadze7@gmail.com>
2025-05-05 01:04:25 +0400

    added admin display

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from finance.models import Budget, Transaction
 # Register your models here.
+
+class TrasnsactionInline(admin.StackedInline):
+    model = Transaction
+    extra = 1
+
+class BudgetAdmin(admin.ModelAdmin):
+    model = Budget
+    list_display = ['id', 'category', 'limit']
+    search_fields = ['category']
+    inlines = [TrasnsactionInline]
+
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ['amount', 'budget_category', 'date']
+
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(Transaction, TransactionAdmin)
