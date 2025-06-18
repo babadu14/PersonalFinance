aee8a7b1b822b8d96dcc15369522f3ed0bd44e32
babadu14 <andriamanagadze7@gmail.com>
2025-06-19 00:26:55 +0400

    added user registration

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),)
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('finance.urls')),
     path("accounts/", include("django.contrib.auth.urls")),  
     path("accounts/", include("users.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]
